881eccb787556f67c91f4fd0154dd59c9b1696ae
AndreVTavares <andretavares3@gmail.com>
2019-11-05 13:26:17 -0300

    Making background of the pokedex and components

---
+++ pokedex-react/src/components/Pokecard/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Pokecard extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+
---
+++ pokedex-react/src/components/Pokedex/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Pokedex = () => {
+    return (
+        <section className="poke-dex">
+            <div>Teste</div>
+        </section>
+    )
+}
+
+export default Pokedex;
+
